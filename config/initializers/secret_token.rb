a7feeaae591b2c8ce644eb89eaa5e62d021fec7c
Roneesh Vashisht <roneesh@gmail.com>
2013-10-23 16:11:34 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SignInEasy::Application.config.secret_key_base = '27b4d20d783c4fa934f15992321c49388ad03a2cf4752d602978f483fd5a0ff51d13def0a5411081e2035e5643a40aeebd2c8b0b57735f8d76a377801ca7e70a'
