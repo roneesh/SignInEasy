be09a0a14d2dfec34f03a95741cc732731762fe4
Roneesh Vashisht <roneesh@gmail.com>
2013-11-02 17:14:19 -0500

    org show page listing guests, but none displaying because new guest records not yet adding org_id

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -2,6 +2,7 @@ class OrganizationsController < ApplicationController
 
   def show
     @organization = Organization.find_by_id(params[:id])
+    @guests = Guest.where(organization_id: @organization.id)
   end
 
 end
