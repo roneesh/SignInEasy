67888ca4cfc428f8b1ee5770d13003bd5f2bf4f7
Roneesh Vashisht <roneesh@gmail.com>
2013-11-02 21:41:56 -0500

    moving all logic of notifications to notification class

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -2,34 +2,8 @@ class Guest < ActiveRecord::Base
   belongs_to :organization
 
   def run_notification_service
-    if email_enabled? && email_allowed?
-      email = Notification::Email.new(self)
-      email.send
-    end
-    
-    if text_enabled? && text_allowed?
-      text = Notification::Text.new(self)
-      text.send
-    end
-
-  end
-
-  private
-
-  def email_enabled?
-    ServiceList.where(organization_id: self.organization_id).email_notification?
-  end
-
-  def email_allowed?
-    #Employee.find_by_id(self.employee_id).allow_email?
-  end
-
-  def text_enabled?
-    ServiceList.where(organization_id: self.organization_id).text_notification?
-  end
-
-  def text_allowed?
-    #Employee.find_by_id(self.employee_id).allow_text?
+    notification = Notifcation.new(self)
+    notification.send
   end
 
 end
--- app/services/notification.rb
+++ app/services/notification.rb
@@ -1,40 +1,34 @@
 class Notification
 
-  class Email
-  end
-
-  class Text
-  end
-
-end
-
-# Email and text notifications only public interface is send
-
-class Notification::Email
-
   def initialize(guest)
-    @destination_email = Employee.find_by_id(guest.employee_id).email
+    @guest = guest
+    @employee = Employee.find_by_id(@guest.employee_id)
+    @services = ServiceList.where(organization_id: @guest.orgnization_id)
   end
 
   def send
-
+    send_email if (services.email_notification?) && email_allowed?
+    send_text if (services.text_notification?) && text_allowed?
   end
 
   private
 
-end
-
-class Notification::Text
-
-  def initialize(guest)
-    @destination_number = Employee.find_by_id(guest.employee_id).phone
+  def email_allowed?
+    @employee.allow_email?
   end
 
-  def send
-
+  def text_allowed?
+    @employee.allow_text?
   end
 
-  private
+  def send_email
+    destination_email = @employee.email
+  end
 
+  def send_text
+    destination_phone = @employee.phone
+    message = "Your guest #{@guest.name.first} has arrived!"
+  end
 
 end
+
