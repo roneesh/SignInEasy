fb4b7349197edc42fdd6e46d0a56c41667124d7c
Roneesh Vashisht <roneesh@gmail.com>
2013-11-02 20:40:10 -0500

    adding methods to guest file that consider if the member wants emails or text

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -2,19 +2,32 @@ class Guest < ActiveRecord::Base
   belongs_to :organization
 
   def run_notification_service
-    if email_enabled?
-      Notification::Email.new(self)
-    elsif text_enabled? 
-      Notification::Text.new(self)
+    if email_enabled? && email_allowed?
+      # Notification::Email.new(self)
     end
+    
+    if text_enabled? && text_allowed?
+      # Notification::Text.new(self)
+    end
+
   end
 
+  private
+
   def email_enabled?
-    ServiceList.where(organization_id: self.organization_id).email_notification
+    ServiceList.where(organization_id: self.organization_id).email_notification?
+  end
+
+  def email_allowed?
+    #Employee.find_by_id(self.employee_id).allow_email?
   end
 
   def text_enabled?
-    ServiceList.where(organization_id: self.organization_id).text_notification
+    ServiceList.where(organization_id: self.organization_id).text_notification?
+  end
+
+  def text_allowed?
+    #Employee.find_by_id(self.employee_id).allow_text?
   end
 
 end
