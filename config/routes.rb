33914fe56b84478a2e2a9d281015aa6255656884
Brian Martinek <brian@brianmartinek.com>
2013-11-07 14:40:15 -0600

    formatting sign in page with bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ SignInEasy::Application.routes.draw do
 
   resources :organizations, only: [:show] do
     resource :member_directory, only: [:show]
-    resources :guests, only: [:new]   
+    resources :guests, only: [:new]  
+    get "static_pages/dashboard" 
   end
   resources :guests, only: [:create]
 
