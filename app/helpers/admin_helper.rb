589d05fd467d7d17c36612f099ed087a60c3706f
Roneesh Vashisht <roneesh@gmail.com>
2013-11-09 15:50:23 -0600

    admin page pulling only important attributes and column names

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,6 +1,12 @@
 module AdminHelper
 
-def important_columns
+def important_columns(object)
+  column_names = object.column_names
+  column_names - ["created_at", "updated_at"]
+end 
+
+def important_attributes(object)
+  object.attributes.delete_if { |key, value| key == "created_at" || key == "updated_at"}
 end
 
 end
