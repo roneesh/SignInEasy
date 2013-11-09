4a90f142866f689442c8d7138e4b250be70f62d7
Roneesh Vashisht <roneesh@gmail.com>
2013-11-08 20:01:15 -0600

    admin controller has methods to constantize a list of classes and fetch all their rows from the db

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,19 @@
 class AdminController < ApplicationController
   def show
+    @objects = all_objects
+  end
+
+  private
+
+  def administrated_classes
+    ['Organization', 'User', 'OrganizationUser', 'Employee', 'ServiceList']
+  end
+
+  def all_objects
+    array = []
+    administrated_classes.each_with_object(array) do |klass, array|
+      array << klass.constantize.all
+    end
+    array
   end
 end
