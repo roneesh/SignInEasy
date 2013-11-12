f3dcf3ae06df8cde0294bfae0c6a7fcadcdde332
Brian Martinek <brian@brianmartinek.com>
2013-11-12 15:01:23 -0600

    fade in of forms working

--- app/assets/javascripts/visitor-form.js
+++ app/assets/javascripts/visitor-form.js
@@ -2,33 +2,37 @@
 $('.visitor-ui-form.a').ready(function(){
 
   $('.meeting-btn').click(function(){
-    $('.btn-grid').fadeOut('fast');
+    $('.btn-grid').fadeOut('fast')
+    $('.form-with-host').fadeIn('fast');
   })
-  $()
+  
 
   $('.drop-in-btn').click(function(){
     $('.btn-grid').fadeOut('fast');
+    $('.form-with-host').fadeIn('fast');  
   })
-  $()
+  
 
   $('.tour-btn').click(function(){
     $('.btn-grid').fadeOut('fast');
+    $('.form-with-host').fadeIn('fast');
   })
-  $()
+
 
   $('.event-btn').click(function(){
     $('.btn-grid').fadeOut('fast');
+    $('.form-with-host').fadeIn('fast');
   })
-  $()
 
   $('.info-btn').click(function(){
     $('.btn-grid').fadeOut('fast');
+    $('.form-with-host').fadeIn('fast');
   })
-  $()
+
 
   $('.other-btn').click(function(){
     $('.btn-grid').fadeOut('fast');
+    $('.form-with-host').fadeIn('fast');
   })
-  $()
 
 })
