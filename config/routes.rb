15ce20d1ba89361bf755e3032b394563824004dd
Brian Martinek <brian@brianmartinek.com>
2013-11-12 10:23:51 -0600

    deleted additional pages on form

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,7 @@ SignInEasy::Application.routes.draw do
 
   resources :organizations, only: [:show] do
     resource :member_directory, only: [:show]
-    resources :guests, only: [:new, :index] 
-    get 'static_pages/dashboard'
-    get 'guests/new_with_host', to: 'guests#new'
-    get 'guests/new_without_host', to: 'guests#new'
-    get 'guests/start'
+    resources :guests, only: [:new, :index]
 
 
   end
