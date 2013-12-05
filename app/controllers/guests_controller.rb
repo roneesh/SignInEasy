c3437b8f4070e3d26260433e97c5211072bda583
Brian Martinek <brian@brianmartinek.com>
2013-12-05 15:20:24 -0600

    guest new --> show page loop working

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -1,7 +1,12 @@
 class GuestsController < ApplicationController
 
+
+  
   layout "visitor_ui", :only => ["new", "show"]
   
+  before_action :set_organization
+
+
   def index
     @guests = Guest.page(params[:page]).per_page(100).order("created_at DESC")
   end
@@ -17,17 +22,18 @@ class GuestsController < ApplicationController
   end
 
   def create
-    guest = Guest.new(guest_params)
-    guest.set_id
-    if guest.save
-      guest.run_notification_service if guest.employee_id
-      redirect_to organization_guest_path(params[:guest][:organization_id])
+    @guest = Guest.new(guest_params)
+    @guest.set_id
+    if @guest.save
+      @guest.run_notification_service if @guest.employee_id
+      redirect_to organization_guest_path(@guest.organization_id, @guest.id)
     else
-      redirect_to organization_guest_path(params[:guest][:organization_id])
+      redirect_to organization_guest_path(@guest.organization_id, @guest.id)
     end
   end
 
   def show
+    @guest = Guest.find(params[:id])
   end
 
   def destroy
@@ -38,7 +44,9 @@ class GuestsController < ApplicationController
 
   private
 
-  
+  def set_organization
+    @organization = Organization.find_by_id(params[:organization_id])
+  end
 
   def guest_params
     params.require(:guest).permit(:name, :email, :company, :reason, :organization_id, :employee_name)
