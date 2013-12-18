b682e928c7ac8210c4577c83f888e6e5f9f5eed4
Roneesh Vashisht <roneesh@gmail.com>
2013-12-18 17:06:08 -0600

    OrganizationUser is now unique to the table and not just a scope of org_id

--- app/models/organization_user.rb
+++ app/models/organization_user.rb
@@ -2,5 +2,5 @@ class OrganizationUser < ActiveRecord::Base
   belongs_to :user
   belongs_to :organization
 
-  validates :user_id, uniqueness: { scope: :organization_id}
+  validates :user_id, uniqueness: true
 end
