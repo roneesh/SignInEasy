592c4010b39dfb20721e57a7c4ed0bb8270b69cf
Roneesh Vashisht <roneesh@gmail.com>
2013-12-23 13:11:50 -0600

    quick seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,24 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+User.destroy_all
+Employee.destroy_all
+Organization.destroy_all
 
+u = User.create(email: "r@e.com", password: "1", name: "Roneesh Vashisht")
 
-Organization.create(name: "1871")
+o = Organization.create(name: "1871", admin_id: User.find_by_email("r@e.com"))
 
 employees = [
-  {name: "Roneesh Vashisht", company: "1871", email: "r@e.com", phone: "4693379220", organization_id: 1},
-  {name: "Brian Martinek", company: "1871", email: "b@e.com", phone: "4693379410", organization_id: 1},
-  {name: "Jeff Lunsford", company: "1871", email: "j@e.com", phone: "4693379887", organization_id: 1},
-  {name: "Kelly Livermore", company: "1871", email: "k@e.com", phone: "4693379001", organization_id: 1},
-  {name: "Michele Laubenthal", company: "1871", email: "m@e.com", phone: "4693379344", organization_id: 1},
-  {name: "Andrew Tardiff", company: "1871", email: "a@e.com", phone: "4693379123", organization_id: 1}
+  {name: "Roneesh Vashisht", company: "1871", email: "r@e.com", phone: "4693379220", organization_id: o.id},
+  {name: "Brian Martinek", company: "1871", email: "b@e.com", phone: "4693379410", organization_id: o.id},
+  {name: "Jeff Lunsford", company: "1871", email: "j@e.com", phone: "4693379887", organization_id: o.id},
+  {name: "Kelly Livermore", company: "1871", email: "k@e.com", phone: "4693379001", organization_id: o.id},
+  {name: "Michele Laubenthal", company: "1871", email: "m@e.com", phone: "4693379344", organization_id: o.id},
+  {name: "Andrew Tardiff", company: "1871", email: "a@e.com", phone: "4693379123", organization_id: o.id}
 ]
 
 employees.each do |employee|
   Employee.create(employee)
 end
+
