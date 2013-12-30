e2a1b6a9ddec4515cfccbbdbf9f01bc415fb97e8
Brian Martinek <brian@brianmartinek.com>
2013-12-30 13:40:20 -0600

    added pagination to employees/index page @ 100 records

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -9,7 +9,8 @@ class EmployeesController < ApplicationController
   end
 
   def index
-    @employees = Employee.where(organization_id: params[:organization_id])
+    @employees = Employee.where(organization_id: params[:organization_id]).page(params[:page]).per_page(100).order("created_at DESC")
+
   end
 
   def edit
