d42c3b2582b80fcd47583e62e77023ed02b7e6c5
Roneesh Vashisht <roneesh@gmail.com>
2013-12-31 16:38:06 -0600

    changing notice message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
 
   def must_be_part_of_organization
     if params[:organization_id] && (params[:organization_id].to_i != current_user.organization.id)
-      redirect_to organization_guests_path(current_user.organization.id), notice: "You can't go there!"
+      redirect_to organization_guests_path(current_user.organization.id), notice: "You can only view information for your organization."
     end
   end
 
