6688666c04a88dad4cddac31d2719170df17cb4d
Roneesh Vashisht <roneesh@gmail.com>
2013-12-31 16:12:32 -0600

    fixing typo in guests controller

--- app/controllers/guests_controller.rb
+++ app/controllers/guests_controller.rb
@@ -36,7 +36,7 @@ class GuestsController < ApplicationController
   def create
     @guest = Guest.new(guest_params)
     @guest.set_id
-    @guest.semployee_name = params[:employee_name]
+    @guest.employee_name = params[:employee_name]
     if @guest.save
       @guest.run_notification_service if @guest.employee_id
       redirect_to organization_guest_path(@guest.organization_id, @guest.id)
