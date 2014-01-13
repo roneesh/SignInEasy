23ac085c03cb9f8bd9dce3188c55e2389cbbd653
Roneesh Vashisht <roneesh@gmail.com>
2014-01-13 13:27:30 -0600

    extending cookie expiration

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,5 +1,6 @@
 Clearance.configure do |config|
   config.mailer_sender = 'donotreply@signineasy.co'
   config.redirect_url = '/profile'
+  config.cookie_expiration = lambda { |cookies| 4.weeks.from_now.utc }
 end
 
